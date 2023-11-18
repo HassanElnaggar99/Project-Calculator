d71818e462ae6a417b0cb97bf9a2110730ea56e1
HassanAlnaggar99 <hassanalnaggar2024@gmail.com>
2023-11-18 02:21:29 +0200

    Added some js functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let num1, op, num2;
+
 const operators = document.querySelector("#operators");
 operators.addEventListener("mousedown", function(e) {
   let btn = e.target;
@@ -20,3 +22,36 @@ operators.addEventListener("mouseout", function(e) {
     btn.style.cssText = "background-color: #fff";
   }
 });
+
+operators.addEventListener("click", function(e) {
+  const results = document.querySelector("#results");
+
+  let btn = e.target, id = btn.id;
+  if (btn.getAttribute('class') === 'operand') {
+    // results.textContent += results.id;
+    switch(btn.id) {
+      case "btn_0":
+      case "btn_1":
+      case "btn_2":
+      case "btn_3":
+      case "btn_4":
+      case "btn_5":
+      case "btn_6":
+      case "btn_7":
+      case "btn_8":
+      case "btn_9":
+      case "btn_.":
+        results.textContent += id[id.length - 1];
+        break;
+      case "btn_back":
+        results.textContent = results.textContent.slice(0, -1);
+        break;
+      case "btn_clear":
+        results.textContent = "";
+        break;
+
+    }
+  }
+});
+
+const results = document.querySelector("#results");
